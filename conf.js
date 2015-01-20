88746a08b12e9688d92bc0db00e87b495907e2b2
Kevalin <kevalin123@gmail.com>
2015-01-20 11:07:50 +0800

    version 001

---
+++ conf.js
@@ -0,0 +1,12 @@
+exports.options = {
+    count      : 10,
+    interval   : 1,
+    packetsize : 64,
+    timeout    : 2000,
+    ip : [
+        "223.5.5.5",
+        "223.6.6.6",
+        "8.8.8.8",
+        "114.114.114.114",
+    ]
+}
